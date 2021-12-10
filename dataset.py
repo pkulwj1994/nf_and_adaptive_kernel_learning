eb8b2fa5c9410ca99dd33b3dc769018d521f7d72
William Luo <32350533+pkulwj1994@users.noreply.github.com>
2021-12-10 23:31:23 +0800

    Add files via upload

--- dataset.py
+++ dataset.py
@@ -9,14 +9,30 @@ torch.save(DataSets.train.data,'./crescent_train.pt')
 
 
 
-a = torch.load('./crescent_test.pt')
+DataSets = MixtureOfGaussian5(train_samples=100000, test_samples=50000)
+
+
+torch.save(DataSets.test.data,'./mog5_test.pt')
+torch.save(DataSets.train.data,'./mog5_train.pt')
 
 
 
 
 
+DataSets = MixtureOfGaussian5highlow(train_samples=100000, test_samples=50000)
 
 
+torch.save(DataSets.test.data,'./mog5hl_test.pt')
+torch.save(DataSets.train.data,'./mog5hl_train.pt')
+
+vecs = DataSets.test.data[0:2000]
+
+
+fig = plt.figure()
+plt.scatter(vecs[:,0],vecs[:,1],s=1,label='mog4')
+plt.legend()
+# scatter_fig = fig.get_figure()
+plt.savefig(os.path.join('./assets','mog5hl.png'), dpi = 400)
 
 
 
